68814d33681266e68061d287a2dce5947710d628
chittacchi <flufluflum@gmail.com>
2025-08-16 22:36:49 +0900

    holiday.jsの修正

--- kadai_010/holiday.js
+++ kadai_010/holiday.js
@@ -6,6 +6,8 @@ for ( let i = 0; i < holidays.length; i++) {
 }
 
 // while文の場合
-while ( holidays.length > 0 ) {
-    console.log(holidays.shift());  
+let i = 0;
+while ( holidays.length > i ) {
+    console.log(holidays[i]);
+    i++;
 }
